18f1a1b914501c1ec19a280b3b5936ffc3bd550e
Dejan Skorupan <dejan.skorupan@catenamedia.com>
2020-08-01 12:29:02 +0200

    Initial commit: Created a html boilerplate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,34 @@
 import React from 'react';
 
-function App() {
-  return <div>Implement me!</div>;
+export function App(props) {
+  return (
+    <div className="app">
+      <form>
+        <input
+          name="emails"
+          type="file"
+          accept=".txt"
+          multiple
+        />
+
+        <ul className="app__selected-files">
+          <li>firstitem.txt</li>
+          <li>seconditem.txt</li>
+          <li>thirditem.txt</li>
+        </ul>
+
+        <button type="submit">Upload</button>
+
+        <div>Emails sent successfully!</div>
+
+        <div>There was an error: Failed to send emails to some addresses</div>
+        <ul>
+          <li>firstitem.txt</li>
+          <li>seconditem.txt</li>
+        </ul>
+      </form>
+    </div>
+  );
 }
 
 export default App;
